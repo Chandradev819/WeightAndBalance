ae6a4f4d0431f5c9610275aa5d15e4dcddd500c7
Chandradev <chandradev819@hotmail.com>
2021-08-11 15:03:22 +0530

    Add project files.

---
+++ Data/Service/EmpService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+
+namespace WeightAndBalance.Service
+{
+    public class EmpService:IEmp
+    {
+        private readonly HttpClient httpClient;
+        public EmpService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public string SayHello()
+        {
+            return "Hello";
+        }
+    }
+}
